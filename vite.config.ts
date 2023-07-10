864e086ae11d5b2c264ab60b5fee9927a9f4d2b7
Liliya <liliyavoloshine@gmail.com>
2023-07-10 17:58:45 +0600

    feat: make pwa

--- vite.config.ts
+++ vite.config.ts
@@ -2,51 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import * as path from 'path'
 import svgr from 'vite-plugin-svgr'
-import { VitePWA } from 'vite-plugin-pwa'
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa'
+import manifest from './public/manifest.json'
+
+const manifestForPlugin: Partial<VitePWAOptions> = {
+  registerType: 'prompt',
+  includeAssets: ['favicon.ico'],
+  manifest: manifest
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    svgr(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      injectRegister: 'auto',
-      manifest: {
-        theme_color: '#7D9AFF',
-        background_color: '#7D9AFF',
-
-        display: 'browser',
-        scope: '/',
-        start_url: '/',
-        name: 'Brain EQ',
-        short_name: 'BrainEQ',
-        description: 'Binaural beats player',
-        icons: [
-          {
-            src: '/icon-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: '/icon-256x256.png',
-            sizes: '256x256',
-            type: 'image/png'
-          },
-          {
-            src: '/icon-384x384.png',
-            sizes: '384x384',
-            type: 'image/png'
-          },
-          {
-            src: '/icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-    })
-  ],
+  plugins: [react(), svgr(), VitePWA(manifestForPlugin)],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }]
   }
